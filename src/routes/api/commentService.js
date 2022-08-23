55da51831526122dce5ca9d1ecbee4b01b7920b7
gianluca <giriboldi@deloitte.it>
2022-08-23 14:49:36 +0200

    [Router] comments

---
+++ src/routes/api/commentService.js
@@ -0,0 +1,20 @@
+const { Router } = require('express')
+const router = Router()
+
+router
+  .get('/:id', (req, res) => {
+    const { id } = req.params
+    res.send(`GET single comment with id: ${id}`)
+  })
+  .get('/', (req, res) => {
+    res.send(`GET all comments written`)
+  })
+  .post('/', (req, res) => {
+    res.send(`POST with this body: ${req.body.text}`)
+  })
+  .put('/:id', (req, res) => {
+    const { id } = req.params
+    res.send(`PUT to mody single comment with id: ${id}`)
+  })
+
+module.exports = router
