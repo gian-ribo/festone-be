5c44b8d78222ce7307d201785f61aeeffef3d0ab
gianluca <giriboldi@deloitte.it>
2022-08-24 15:07:57 +0200

    [slonik] pg config and init

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
-const comments = require('./routes/api/comments')
-const PORT = process.env.PORT || 3000
+const { createPool } = require('slonik')
 
-const app = express()
+;(async () => {
 
-app
-  .use(bodyParser.json())
-  .use('/api/comments', comments)
-  .get('/', (req, res) => {
-    res.send('Up & Running')
+  const PORT = process.env.PORT || 3000
+  const app = express()
+
+  const pool = await createPool(`${process.env.DATABASE_URL}?sslmode=no-verify`, {
+    maximumPoolSize: 50
   })
-  .listen(PORT, () => console.log(`Now listening on port ${PORT}`))
+
+  app
+    .use(bodyParser.json())
+    .use('/api/comments', require('./routes/api/comments')(pool))
+    .get('/', (req, res) => {
+      res.send('Up & Running')
+    })
+    .listen(PORT, () => console.log(`Now listening on port ${PORT}`))
+})()
