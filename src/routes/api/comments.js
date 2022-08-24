f6217ce6ea5d95e97e5545c43abae6d262be6267
gianluca <giriboldi@deloitte.it>
2022-08-24 15:08:14 +0200

    [Comments] get all, getById, insert

--- src/routes/api/comments.js
+++ src/routes/api/comments.js
@@ -1,21 +1,62 @@
 const { Router } = require('express')
-const { createPool } = require('slonik')
 const router = Router()
+const { sql } = require('slonik')
 
-router
-  .get('/:id', (req, res) => {
-    const { id } = req.params
-    res.send(`GET single comment with id: ${id}`)
-  })
-  .get('/', (req, res) => {
-    res.send('GET all comments written')
-  })
-  .post('/', (req, res) => {
-    res.send(`POST with this body: ${req.body.text}`)
-  })
-  .put('/:id', (req, res) => {
-    const { id } = req.params
-    res.send(`PUT to mody single comment with id: ${id}`)
-  })
+const fetchComments = async (req, res, pool) => {
+  return await pool.any(sql`
+    SELECT *
+    FROM comment`)
+}
 
-module.exports = router
+const fetchComment = async (req, res, pool) => {
+  const { id } = req.params
+  let result = {}
+  try {
+    result = await pool.one(sql`
+      SELECT *
+      FROM comment
+      WHERE id = ${id}`)
+  } catch(e) {}
+
+  return result
+}
+
+const createComment = async (req, res, pool) => {
+  const { name, text } = req.body
+  let error = ''
+  let result = {}
+
+  if(!text) {
+    res.statusCode = 400
+    error = 'Incorrect JSON format: text is null'
+    return { result, error }
+  }
+
+  r = await pool.query(sql`INSERT INTO comment(name, text)
+    VALUES(${name}, ${text})
+    RETURNING id`)
+
+  result = r.rows[0]
+
+  return { result, error }
+}
+
+module.exports = pool => router
+  .get('/:id', async (req, res) => {
+    const rows = await fetchComment(req, res, pool)
+
+    res.setHeader('content-type', 'application/json')
+    res.send(rows)
+  })
+  .get('/', async (req, res) => {
+    const rows = await fetchComments(req, res, pool)
+
+    res.setHeader('content-type', 'application/json')
+    res.send(rows)
+  })
+  .post('/', async (req, res) => {
+    let r = await createComment(req, res, pool)
+
+    res.setHeader('content-type', 'application/json')
+    res.send(r.error ? { error: r.error } : r.result)
+  })
